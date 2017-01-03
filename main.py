303bf9f4f879abf1714fbb372583faed94b83f87
Kevin W <gra230434@gmail.com>
2017-01-03 13:15:41 +0800

    ver1.2.003

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from classes.file_create import create_files
 
+filesize = '5'
+fileunit = 'MB'
+filequantity = 1000
+
 # 111_nfs
-create1 = create_files('home', 'cloud', '111_nfs', '5KB')
-create1.CreateBegin(5, 'KB')
+create1 = create_files('home', 'cloud', '111_nfs', filesize + fileunit)
+create1.CreateBegin(filesize, fileunit)
 create1.InitCreate(run='on')
-create1.CreateFile(number=15000)
+create1.CreateFile(number=filequantity)
 # 70_nfs
-create2 = create_files('home', 'cloud', '70_nfs', '5KB')
-create2.CreateBegin(5, 'KB')
+create2 = create_files('home', 'cloud', '70_nfs', filesize + fileunit)
+create2.CreateBegin(filesize, fileunit)
 create2.InitCreate(run='on')
-create2.CreateFile(number=15000)
+create2.CreateFile(number=filequantity)
 # 70_v001
-create3 = create_files('home', 'cloud', '70_v001', '5KB')
-create3.CreateBegin(5, 'KB')
+create3 = create_files('home', 'cloud', '70_v001', filesize + fileunit)
+create3.CreateBegin(filesize, fileunit)
 create3.InitCreate(run='on')
-create3.CreateFile(number=15000)
+create3.CreateFile(number=filequantity)
 # samba_mount
-create4 = create_files('home', 'cloud', 'samba_mount', '5KB')
-create4.CreateBegin(5, 'KB')
+create4 = create_files('home', 'cloud', 'samba_mount', filesize + fileunit)
+create4.CreateBegin(filesize, fileunit)
 create4.InitCreate(run='on')
-create4.CreateFile(number=15000)
+create4.CreateFile(number=filequantity)
